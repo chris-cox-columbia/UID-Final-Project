ed92d9b09d4e16a4787009e8a140c501cac3fe29
Julia Doherty <julia.doherty@leaftech.eu>
2022-04-16 22:07:42 -0400

    framework for learning templates & new database info

--- database.py
+++ database.py
@@ -11,76 +11,87 @@ drinksData = {"1":{"id":"1",
                                             "hexcode":"#DCFF00"
                                             },
                                     "Ginger Beer":{
-                                                    "quantity":"Two parts",
-                                                    "hexcode":"#FFB200"
-                                                    },
+                                            "quantity":"Two parts",
+                                            "hexcode":"#f07c3c"
+                                            },
                                     "Vodka":{
                                             "quantity":"One Part",
                                             "hexcode":"#FFFFFF"
-                                        }
+                                            }
                                     },
-                    "procedure":["Add Ginger Beer and Vodka together at a ratio of 2:1",
+                    "procedure":["Add Ginger Beer and Vodka together",
                                  "Garnish with a lime slice optionally squeezing some lime juice inside",
                                  "Serve with chilled ice"],
-                    "emphasize":{"procedure_1":["Ginger Beer","Vodka"],"procedure_2":["lime slice"]},
+                    "emphasize":{"1":["Ginger Beer", "Vodka"], "2":["lime slice"]},
                     "next":"2"
                     },
                 "2":{"id":"2",
                     "name":"Campari Flamingo",
                     "description":["Campari is often used in cocktails and is commonly served with soda water or citrus juice",
                                     "Campari was invented in 1860 by Gaspare Campari in Novara, Italy. It was originally coloured with carmine dye, derived from crushed cochineal insects, which gave the drink its distinctive red colour",
-                                    "Campari is a registered trademark of Davide Campari Milano S.P.A.,[2][3] which is part of Gruppo Campari (Campari Group)"],
+                                    "Campari is a registered trademark of Davide Campari Milano S.P.A., which is part of Gruppo Campari (Campari Group)"],
                     "ingredient_number":"four",
-                    "ingrdient_list":["Grapefruit Soda","Rum","Lime Juice","Campari"],
+                    "ingredient_list":["Grapefruit Soda","Rum","Lime Juice","Campari"],
                     "ingredient_details":{
                                     "Grapefruit Soda":{
-                                                    "quantity":"Four parts",
-                                                    "hexcode":"#EE84FE"
-                                                    },
-                                    "Rum":{
-                                            "quantity":"Three parts",
-                                            "hexcode":"#D2801F"
+                                            "quantity":"Four parts",
+                                            "hexcode":"#eabceb"
                                             },
-                                    "Lime Juice":{
-                                            "quantity":"Two parts",
-                                            "hexcode":"#92FF0D"
-                                        },
-                                    "Campari":{
-                                            "quantity":"One part",
-                                            "hexcode":"#FF000"
-                                        }
-                                    },
-                    "procedure":["Fill a cocktail shaker with ice. Add lime juice, rum, and Campari. Shake well.",
-                                 "Strain into ice-filled rocks glass. Add grapefruit soda, stir gently and serve"],
-                    "next":"3"
-                    },
-                "3":{"id":"3",
-                    "name":"Vodka Martini",
-                    "description":["Before its silver screen appearance, the Martini Cocktail was featured in the 1958 book Dr. No by Ian Fleming.",
-                                    "The original martini cocktails were made with Old Tom gin, sweet vermouth, bitters and a little bit of maraschino liqueur.",
-                                    "In the 80s and 90s flavor was king. This is where we saw the birth of some phenomenal classic cocktails such as the iconic Espresso Martini Cocktail, the Cosmopolitan and more. "],
-                    "ingredient_number":"three",
-                    "ingrdient_list":["Vodka","Dry vermouth","Olives"],
-                    "ingredient_details":{
-                                    "Vodka":{
+                                    "Rum":{
                                             "quantity":"Three parts",
                                             "hexcode":"#FFFFFF"
                                             },
-                                    "Dry vermouth":{
+                                    "Lime Juice":{
+                                            "quantity":"Two parts",
+                                            "hexcode":"#dbeb74"
+                                        },
+                                    "Campari":{
                                             "quantity":"One part",
-                                            "hexcode":"#EAE8B6"
-                                            },
-                                    "Olives":{
-                                            "quantity":"Garnish",
-                                            "hexcode":"#92FF0D"
+                                            "hexcode":"#e93b33"
                                         }
                                     },
-                    "procedure":["Freeze martini glass 15 minutes before making the drink.",
-                                 "Pour the vodka and vermouth into a  mixing glass or cocktail shaker filled with ice. Stir vigorously, about 20 seconds, until well chilled.",
-                                 "Strain into a martini glass, garnish with olive or lemon peel and serve."],
+                    "procedure":["Fill a cocktail shaker with ice.",
+                                 "Add lime juice, rum, and Campari. Shake well.",
+                                 "Strain into ice-filled rocks glass.",
+                                 "Add grapefruit soda, stir gently and serve"],
+                    "emphasize":{"1":["lime juice", "rum", "Campari"],"2":["grapefruit soda"]},
+                    "next":"3"
+                    },
+                "3":{"id":"3",
+                    "name":"Mai Tai",
+                    "description":["While it’s impossible to give full credit to one maker of the Mai Tai, its invention is likely a one-two punch of two Tiki icons: Ernest Raymond Beaumont Gantt (aka Donn Beach) and Victor “Trader Vic” Bergeron.",
+                                    "The history of the Mai Tai came out of people’s yearning for a place that’s carefree and peaceful after the Great Depression",
+                                    "“Maita’i” is the Tahitian word for “good”"],
+                    "ingredient_number":"five",
+                    "ingredient_list":["White Rum","Curaçao","Lime Juice", "Dark Rum", "Orgeat"],
+                    "ingredient_details":{
+                                    "White Rum":{
+                                            "quantity":"Four parts",
+                                            "hexcode":"#FFFFFF"
+                                            },
+                                    "Curaçao":{
+                                            "quantity":"Two parts",
+                                            "hexcode":"#45b3f1"
+                                            },
+                                    "Lime Juice":{
+                                            "quantity":"Two parts",
+                                            "hexcode":"#dae874"
+                                        },
+                                    "Dark Rum":{
+                                            "quantity":"One part",
+                                            "hexcode":"#6b2f18"
+                                        },
+                                    "Orgeat":{
+                                            "quantity":"One part",
+                                            "hexcode":"#fde699"
+                                        }
+                                    },
+                    "procedure":["Add the white rum, curaçao, lime juice and orgeat into a shaker with crushed ice and shake lightly (about 3 seconds).",
+                                 "Pour into a double rocks glass",
+                                 "Float the dark rum over the top."],
+                    "emphasize":{"1":["white rum","curaçao", "lime juice", "orgeat"],"3":["dark rum"]},
                     "next":""
-                    }
-            }
+                    }}
 questions = {"1":{"id":"1",
                   "type":"drag and drop",
                   "question":"Drag the ingredients for a Moscow Mule into the shaker.",
@@ -103,7 +114,7 @@ questions = {"1":{"id":"1",
                             "Ginger Beer",
                             "Lime Slice",],
                     "next":"2"
-                    },   
+                    },
             "2":{"id":"2",
                   "type":"ratios",
                   "question":"Give the ingredient ratio in a Moscow Mule (garnishes have ratio of 0):",
@@ -114,7 +125,7 @@ questions = {"1":{"id":"1",
                             ],
                     "answer":[2,0,1],
                     "next":"3"
-                    }, 
+                    },
             "3":{"id":"3",
                   "type":"free form",
                   "question":"List the ingredients and their ratios of a Moscow Mule: (not all boxes must be used)",
@@ -123,7 +134,7 @@ questions = {"1":{"id":"1",
                             "Lime Slice":"0",
                             "Gin":"0"},
                    "next":"4"
-                },       
+                },
             "4":{"id":"4",
                   "type":"drag and drop",
                   "question":"Drag the ingredients for a Campari Flmaingo into the shaker.",
@@ -147,7 +158,7 @@ questions = {"1":{"id":"1",
                             "Rum",
                             "Grapefruit Soda"],
                     "next":"5"
-                    },   
+                    },
             "5":{"id":"5",
                   "type":"ratios",
                   "question":"Give the ingredient ratio in a Campari Flamingo (garnishes have ratio of 0):",
@@ -159,7 +170,7 @@ questions = {"1":{"id":"1",
                             ],
                     "answer":[4,3,2,1],
                     "next":"6"
-                    }, 
+                    },
             "6":{"id":"6",
                   "type":"free form",
                   "question":"List the ingredients and their ratios of a Campari Flamingo: (not all boxes must be used)",
@@ -167,48 +178,52 @@ questions = {"1":{"id":"1",
                   "answer":{"Ginger Beer":"2",
                             "Lime Slice":"0",
                             "Gin":"0"},
-                   "next":"4"
-                }, 
+                   "next":"7"
+                },
             "7":{"id":"7",
                   "type":"drag and drop",
-                  "question":"Drag the ingredients for a Vodka Martini into the shaker.",
+                  "question":"Drag the ingredients for a Mai Tai into the shaker.",
                   "options":["Gin",
                             "Vodka",
                             "Soda Water",
                             "Lemon Juice",
                             "Ginger Beer",
-                            "Lime Slice",
+                            "Orgeat",
                             "Campari",
                             "Tequila",
                             "Grapefruit Soda",
-                            "Rum",
+                            "White Rum",
                             "Lime Juice",
                             "Campari",
                             "Vodka",
-                            "Dry vermouth",
-                            "Olives"],
+                            "Curaçao",
+                            "Dark Rum"],
                     "answer":["Vodka",
                             "Dry Vermouth",
                             "Olives",],
                     "next":"8"
-                    },   
+                    },
             "8":{"id":"8",
                   "type":"ratios",
-                  "question":"Give the ingredient ratio in a Vodka Martini (garnishes have ratio of 0):",
-                  "emphasize":{"Vodka Martini"},
-                  "options":["Vodka",
-                            "Dry Vermouth",
-                            "Olives",],
-                    "answer":[3,1,0],
+                  "question":"Give the ingredient ratio in a Mai Tai (garnishes have ratio of 0):",
+                  "emphasize":{"Mai Tai"},
+                  "options":["Orgeat",
+                            "Dark Rum",
+                            "White Rum",
+                             "Curaçao",
+                             "Lime Juice"],
+                    "answer":[1,1,4,2,2],
                     "next":"9"
-                    }, 
+                    },
             "9":{"id":"9",
                   "type":"free form",
-                  "question":"List the ingredients and their ratios of a Vodka Martini: (not all boxes must be used)",
-                  "emphasize":{"Vodka Martini"},
-                  "answer":{"Vodka":"3",
-                            "Dry Vermouth":"1",
-                            "Olives":"0"},
-                   "next":"4"
-                }, 
+                  "question":"List the ingredients and their ratios of a Mai Tai: (not all boxes must be used)",
+                  "emphasize":{"Mai Tai"},
+                  "answer":{"Orgeat":"1",
+                            "Dark Rum":"1",
+                            "White Rum":"4",
+                            "Curaçao":"2",
+                            "Lime Juice":"2"},
+                   "next":""
+                },
 }
