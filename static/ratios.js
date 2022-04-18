f9f7a4b32d7037c970eaf029f14b1ecee11db7d2
MayaVillasenor <48735125+MayaVillasenor@users.noreply.github.com>
2022-04-17 20:58:56 -0400

    skeleton

--- static/ratios.js
+++ static/ratios.js
@@ -11,12 +11,15 @@ function pushIngredients(){
 }
 
 function addIngredients(object){
-  var x = $("<div>" + object + "</div>")
+  var x = $("<div>" + object + "</div><br>")
+  var y = $('<input type="text" placeholder="e.g. 3" id="'+object+'"></input><br>')
   $("#main_row").append(x)
+  $("#main_row").append(y)
 }
 
 // on ready
 
 $(document).ready(function(){
   initialize(questionDetails)
+  pushIngredients()
 })
